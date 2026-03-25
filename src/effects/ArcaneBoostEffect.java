1ebf35a4e163b63c0b4c2ea9fee33446a877e078
Blaze <yunjingliu07@gmail.com>
2026-03-25 12:09:54 +0800

    Add the concrete class ArcaneBoostEffect

---
+++ src/effects/ArcaneBoostEffect.java
@@ -0,0 +1,42 @@
+package effects;
+
+import domain.Combatant;
+
+public class ArcaneBoostEffect implements StatusEffects{
+    private int turns = 1; // Arcane Boost last for unlimited turns
+
+    @Override
+    public String getName(){
+        return "Arcane Boost Effect";
+    }
+
+    @Override
+    public boolean blockAction(){
+        return false; // Arcane boost do not block actions
+    }
+
+    @Override
+    public int modifyAtk(){
+        return 0; // Arcane boost do not modify atk
+    }
+
+    @Override
+    public int modifyDef(){
+        return 0; // Arcane boost do not modify def
+    }
+
+    @Override
+    public int modifySpd(){
+        return 0; // Arcane boost do not modify spd
+    }
+
+    @Override
+    public void onTick(BattleContext ctx, Combatant target){
+        turns -= 0;
+    }
+
+    @Override
+    public boolean isExpired(){
+        return false; // It's never expired
+    }
+}
