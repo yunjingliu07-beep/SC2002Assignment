8b3778c080160a82ab5400403b68b90a72ca4a69
Blaze <yunjingliu07@gmail.com>
2026-03-27 19:48:26 +0800

    Remove unnecessary parameter

--- src/control/TurnOrderStrategy.java
+++ src/control/TurnOrderStrategy.java
@@ -5,5 +5,5 @@ import domain.Combatant;
 import java.util.List;
 
 public interface TurnOrderStrategy {
-    List<Combatant> determineOrder(BattleContext ctx, List<Combatant> combatants);
+    List<Combatant> determineOrder(BattleContext ctx);
 }
