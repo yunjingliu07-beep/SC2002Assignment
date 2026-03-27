d5924cddb37648fe2119a3bd6294f53589abe5d1
Blaze <yunjingliu07@gmail.com>
2026-03-27 16:36:43 +0800

    Add new abstract method useSpecialSkill

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -1,26 +1,25 @@
 package domain;
 
-import effects.StatusEffects;
 import items.Inventory;
 
 public abstract class Player extends AbstractCombatant{
     protected Inventory inventory;
     protected int specialCooldown;
-    public Player(String name, int hp, int maxHp, int atk, int def, int spd) {
+
+    public Player(String name, int hp, int atk, int def, int spd) {
         super(name, hp, atk, def, spd);
         this.inventory = new Inventory();
-        this.specialCooldown = 3; // Cooldown 3 turns according to guideline
     }
-
-    public abstract Action chooseAction(BattleContext ctx);
 
     public int getSpecialCooldown() {
         return specialCooldown;
     }
 
-    public void setSpecialCooldown() {
+    public void tickSpecialCooldown() {
         if (super.canAct() &&  this.specialCooldown > 0){
             this.specialCooldown--;
         }
     }
+
+    public abstract void useSpecialSkill(boolean consumeCoolDown);
 }
