02fda2f56ef84fbb27619c76bbf014ada7d7bedf
Blaze <yunjingliu07@gmail.com>
2026-03-27 16:36:14 +0800

    Add new item class PowerStone

---
+++ src/items/PowerStone.java
@@ -0,0 +1,19 @@
+package items;
+
+import domain.Player;
+
+public class PowerStone implements Items{
+
+    @Override
+    public String getName(){
+        return "Power Stone";
+    }
+
+    @Override
+    public void apply(BattleContext ctx, Player p){
+        System.out.println(p.getName() + " uses power stone!");
+        System.out.println("Now Triggering special skill without cooldown!");
+        p.useSpecialSkill(false); // Power stone can be used without cooldown
+    }
+
+}
