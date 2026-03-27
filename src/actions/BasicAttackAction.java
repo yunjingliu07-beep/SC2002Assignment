40ff60a61b773627206725e490b4c4f24abf309c
Blaze <yunjingliu07@gmail.com>
2026-03-27 16:56:44 +0800

    Add basic attack action

---
+++ src/actions/BasicAttackAction.java
@@ -0,0 +1,43 @@
+package actions;
+
+import domain.Combatant;
+import domain.Enemy;
+
+public class BasicAttackAction implements Actions {
+
+    private Combatant target;
+
+    public BasicAttackAction(Combatant target) {
+        this.target = target;
+    }
+
+    @Override
+    public String actionName(){
+        return "basic attack";
+    }
+
+    @Override
+    public void execute(BattleContext ctx, Combatant actor){
+        // Calculate dmg dealt.
+        int dmg = Math.max(0, actor.getAtk() - target.getDef());
+
+        // Special Case: If smoke bomb active
+        if (actor instanceof Enemy && ctx.isSmokeBombActive()){
+            dmg = 0;
+        }
+
+        // Report dmg info
+        System.out.println(actor.getName() + " uses basic attack on " + target.getName() + " !");
+        System.out.println("The damage is " + dmg);
+        target.takeDamage(dmg);
+
+        if (!target.isAlive()){
+            System.out.println(actor.getName() + " has defeated "+ target.getName());
+        }
+        else{
+            System.out.println("The current status of the target is:\n" + target.getCurrentAttribute());
+        }
+
+    }
+
+}
