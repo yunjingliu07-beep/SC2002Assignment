82c139f79a88c4c66f5c8f336de02d1b65d34dca
Blaze <yunjingliu07@gmail.com>
2026-03-27 17:20:43 +0800

    update GameUI with new abstract methods to be used

--- src/ui/GameUI.java
+++ src/ui/GameUI.java
@@ -5,7 +5,10 @@ import domain.Combatant;
 import java.util.List;
 
 public interface GameUI {
-    String showMessage(String msg);
-    int chooseOption(String prompt, List<String> choices);
-    Combatant chooseTarget(String prompt, List<Combatant> targets);
+    int choosePlayer();
+    int choosePlayerActions();
+    int chooseTarget();
+    int chooseItems();
+    void showEnemies();
+    void showBattleResult(boolean victory);
 }
